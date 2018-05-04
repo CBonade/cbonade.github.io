d62df93ddc89497f7b6c0b3c4e450e9456e0ed2c
Curt Bonade <cbonade@AdminisatorsMBP.attlocal.net>
2018-05-04 09:48:24 -0400

    Init Commit

---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+<div class="header">
+    <img src="../assets/img/vue.js_logo.svg" class="vue-logo">
+    <img src="../assets/img/tube-logo.png" class="tube-logo">
+</div>
+</template>
+
+<script>
+
+export default {
+
+}
+
+</script>
+
+<style lang="scss">
+
+@import "../assets/scss/header.scss"
+
+</style>
